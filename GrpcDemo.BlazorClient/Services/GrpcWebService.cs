f34f936e14267df8c67a83a91a2e2c43287f2262
issamBoutissante <boutissante.issam.dev@gmail.com>
2024-09-23 19:43:07 +0100

    Add project files.

---
+++ GrpcDemo.BlazorClient/Services/GrpcWebService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+using GrpcDemo.Shared;
+
+namespace GrpcDemo.BlazorClient.Services;
+
+public static class GrpcWebService
+{
+    public static Greeter.GreeterClient GetGrpcClient()
+    {
+        var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+        {
+            HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler())
+        });
+
+        return new Greeter.GreeterClient(channel);
+    }
+}
